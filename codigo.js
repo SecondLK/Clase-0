949b69374a66eb2d993e98046eba3ac42730f777
Segundo Locardi <segundolocardi@gmail.com>
2024-05-19 01:58:20 -0300

    JS Act

---
+++ codigo.js
@@ -0,0 +1,33 @@
+
+let nombre = "Segundo";
+let apellido = "Locardi";
+let NombreCompleto;
+
+function Nombrar(){
+    NombreCompleto = nombre + " " + apellido;
+        console.log(NombreCompleto)
+}
+
+Nombrar();
+
+let numeroA = 10;
+let numeroB= 11;
+let Suma;
+
+function sumar(){
+    Suma = numeroA + numeroB
+        console.log(Suma)
+}
+
+sumar();
+console.log();
+
+let numeroC = 9;
+
+function VolverASUmar(){
+    NuevaSuma = Suma + numeroC
+        console.log(NuevaSuma)
+}
+
+VolverASUmar();
+console.log();
